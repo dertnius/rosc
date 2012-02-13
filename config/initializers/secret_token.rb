3aa02b2f98765d2a10bfc929e00ef41782d10824
David Rivas <drivas.yanez@gmail.com>
2012-02-13 15:05:46 +0100

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosc::Application.config.secret_token = '52fa808da5b111e7c9913bc7f9d5916a5075e5fb8e2108cb9c2e932ac60b9d2aa8066f4b60661b258361bd86fbddef21da2c1908ef3dfb1fc6906000b5a9501b'
